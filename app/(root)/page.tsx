8c30c3b2a1db0d6e4124a7c6fe88fc9fae92d406
Nirav Patel <nnirav.patel7@gmail.com>
2023-10-06 15:08:10 -0400

    User Auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+
+import { UserButton } from "@clerk/nextjs";
+
+// import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  // const onOpen = useStoreModal((state) => state.onOpen);
+  // const isOpen = useStoreModal((state) => state.isOpen);
+  // useEffect(() => {
+  //   if (!isOpen) {
+  //     onOpen();
+  //   }
+  // }, [isOpen, onOpen]);
+  // return null;
+
   return (
     <div>
       {" "}
-      <Button>Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
-}
+};
+
+export default SetupPage;
