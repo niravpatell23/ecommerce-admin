9146c9f1169fcdcaa9aa1b72cab902e8ddb04a7d
Nirav Patel <nnirav.patel7@gmail.com>
2023-10-12 14:04:41 -0400

    store Modal update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,24 +4,24 @@ import { useEffect } from "react";
 import { useParams } from "next/navigation";
 
 import { UserButton } from "@clerk/nextjs";
+import { Modal } from "@/components/ui/modal";
 
-// import { useStoreModal } from "@/hooks/use-store-modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
-  // const onOpen = useStoreModal((state) => state.onOpen);
-  // const isOpen = useStoreModal((state) => state.isOpen);
-  // useEffect(() => {
-  //   if (!isOpen) {
-  //     onOpen();
-  //   }
-  // }, [isOpen, onOpen]);
-  // return null;
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return null;
 
   return (
-    <div>
-      {" "}
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <Modal title="Test" description="asds" isOpen onClose={() => {}}>
+      children
+    </Modal>
   );
 };
 
